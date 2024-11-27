ccf9d0a9f49113bba9418f085b0442e52aa6e7ff
Krzysztof Platis <platonn.git@gmail.com>
2024-11-27 12:06:19 +0100

    in lib1 defer-load component from lib4

--- projects/lib1/src/lib/lib1-a.component.ts
+++ projects/lib1/src/lib/lib1-a.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Lib4AComponent } from 'lib4';
 
 @Component({
   selector: 'lib-lib1-a',
   standalone: true,
-  imports: [],
-  template: ` <p>lib1-a works!</p> `,
+  imports: [Lib4AComponent],
+  template: `
+    <p>lib1-a works!</p>
+
+    <hr />
+
+    4. Defer-loaded local lib4-a-component VIA a simple proxy-file
+    <br />
+    @defer (on interaction) {
+    <lib-lib4-a />
+    } @placeholder {
+    <div>
+      SPIKE click me to load deferred - lib4-a-component VIA a proxy-file
+    </div>
+    }
+
+    <hr />
+  `,
   styles: ``,
 })
 export class Lib1AComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,11 @@ import { Lib2AComponent } from 'lib2';
 import { Lib3AWrapperComponent } from './lib3-a-WRAPPER.component';
 
 // NOTE: it's not imported directly from lib4, but via a local proxy-file
-import { Lib4AComponent } from './lib4-a-WRAPPER-simple-reexport';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    Lib1AComponent,
-    Lib2AComponent,
-    Lib3AWrapperComponent,
-    Lib4AComponent, // imported via a local proxy-file
-  ],
+  imports: [Lib1AComponent, Lib2AComponent, Lib3AWrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
